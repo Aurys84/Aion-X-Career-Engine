59a634faedafd13523933d7337f020deab98ba4f
Csonka Norbert <norbi.markk2013@gmail.com>
2026-04-26 20:58:58 +0200

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -4,11 +4,32 @@ const omniDict = [
     { hu: "Érettségi", en: "High School Diploma", de: "Abitur" },
     { hu: "Jogosítvány", en: "Driving License", de: "Führerschein" },
     { hu: "B kategória", en: "Category B", de: "Klasse B" },
-    { hu: "Plébános", en: "Parish priest", de: "Pfarrer" }
+    { hu: "Személyautó", en: "Passenger car", de: "PKW" }
 ];
 
 const dictionary = {
-    hu: { title: "AION-X PANEL", name: "Név", phone: "Tel:", email: "Email:", zip: "Irsz:", city: "Város", street: "Utca", house: "Házszám", summary: "Profil", work: "Tapasztalat", edu: "Tanulmányok", skills: "Készségek", license: "Jogosítvány", hobby: "Hobbi", photo: "Fotó", style: "Stílus", color: "Szín", addW: "+ Új Munka", addE: "+ Új Iskola", addr: "Cím:" },
-    en: { title: "AION-X PANEL", name: "Name", phone: "Phone:", email: "Email:", zip: "ZIP:", city: "City", street: "Street", house: "No:", summary: "Summary", work: "Experience", edu: "Education", skills: "Skills", license: "License", hobby: "Hobbies", photo: "Photo", style: "Style", color: "Color", addW: "+ Add Job", addE: "+ Add Edu", addr: "Address:" },
-    de: { title: "AION-X PANEL", name: "Name", phone: "Tel:", email: "Email:", zip: "PLZ:", city: "Stadt", street: "Straße", house: "Hausnr:", summary: "Profil", work: "Erfahrung", edu: "Ausbildung", skills: "Kenntnisse", license: "Führerschein", hobby: "Hobby", photo: "Foto", style: "Stil", color: "Farbe", addW: "+ Job", addE: "+ Schule", addr: "Adresse:" }
+    hu: { 
+        title: "AION-X PANEL", name: "Név", phone: "Telefon", email: "Email", 
+        zip: "Irsz", city: "Város", street: "Utca", house: "Házszám", 
+        summary: "Profil", work: "Tapasztalat", edu: "Tanulmányok", 
+        skills: "Készségek", license: "Jogosítvány", style: "Stílus", 
+        color: "Szín", addW: "+ Új Munka", addE: "+ Új Iskola", 
+        addr: "Cím:", print: "PDF GENERÁLÁSA" 
+    },
+    en: { 
+        title: "AION-X PANEL", name: "Name", phone: "Phone", email: "Email", 
+        zip: "ZIP", city: "City", street: "Street", house: "No.", 
+        summary: "Profile Summary", work: "Experience", edu: "Education", 
+        skills: "Skills", license: "License", style: "Style", 
+        color: "Color", addW: "+ Add Job", addE: "+ Add Edu", 
+        addr: "Address:", print: "GENERATE PDF" 
+    },
+    de: { 
+        title: "AION-X PANEL", name: "Name", phone: "Tel", email: "Email", 
+        zip: "PLZ", city: "Stadt", street: "Straße", house: "Hausnr.", 
+        summary: "Profil", work: "Erfahrung", edu: "Ausbildung", 
+        skills: "Kenntnisse", license: "Führerschein", style: "Stil", 
+        color: "Farbe", addW: "+ Job", addE: "+ Schule", 
+        addr: "Adresse:", print: "PDF GENERIEREN" 
+    }
 };

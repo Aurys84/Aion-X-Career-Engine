06e925c4657663abe875410e11d2c64d9c565574
Csonka Norbert <norbi.markk2013@gmail.com>
2026-04-26 22:35:17 +0200

    Refactor update functions for better clarity
    Refactor updatePreview and updateInterface functions, improving label handling and removing unused code.

--- engine.js
+++ engine.js
@@ -21,67 +21,50 @@ function updatePreview() {
     const szin = document.getElementById('theme-color').value;
     document.documentElement.style.setProperty('--main-color', szin);
     
-    // NÉV ÖSSZERAKÁSA NYELV SZERINT
+    // NÉV LOGIKA: Tűpontos sorrendvezérlés
     const vez = document.getElementById('in-lastName').value;
     const ker = document.getElementById('in-firstName').value;
+    
     let fullName = "";
     if (vez || ker) {
+        // Ha magyar, akkor Vezetéknév + Keresztnév, különben fordítva
         fullName = (currentLang === 'hu') ? vez + " " + ker : ker + " " + vez;
     }
+    
     document.getElementById('out-name').innerText = fullName.toUpperCase() || "NAME";
     document.getElementById('out-name').style.color = szin;
 
     renderAsync(szin);
 }
 
-async function renderAsync(szin) {
-    const d = dictionary[currentLang];
-    const city = await deepTranslate(document.getElementById('in-city').value);
-    
-    // UTCA TÍPUS FORDÍTÁSA A SZÓTÁRBÓL
-    const sName = document.getElementById('in-street-name').value;
-    const sTypeHU = document.getElementById('in-street-type').value;
-    const sTypeTranslated = omniDict.find(e => e.hu === sTypeHU)[currentLang];
-    const fullStreet = sName + " " + sTypeTranslated;
-    
-    const zip = document.getElementById('in-zip').value;
-    const house = document.getElementById('in-house').value;
-
-    document.getElementById('out-contact').innerHTML = `
-        <div style="margin-top:10px;">
-            <b>${d.phone}</b> ${document.getElementById('in-phone').value} | <b>${d.email}</b> ${document.getElementById('in-email').value}<br>
-            <b>${d.addr}</b> ${currentLang === 'hu' ? city+', '+fullStreet+' '+house+', '+zip : zip+' '+city+', '+fullStreet+' '+house}
-        </div>
-    `;
-
-    let html = "";
-    const sum = await deepTranslate(document.getElementById('in-summary').value);
-    if(sum) html += `<h3>${d.summary}</h3><p>${sum}</p>`;
-    const lic = await deepTranslate(document.getElementById('in-license').value);
-    if(lic) html += `<h3>${d.license}</h3><p>${lic}</p>`;
-
-    for (let type of ['edu', 'work']) {
-        let items = "";
-        const boxes = document.querySelectorAll('#' + type + '-container .entry-box');
-        for (let box of boxes) {
-            const m = await deepTranslate(box.querySelector('.e-main').value);
-            const sub = box.querySelector('.e-sub').value;
-            const desc = await deepTranslate(box.querySelector('.e-desc').value);
-            if(m || desc) items += `<div style="margin-bottom:12px"><b>${m}</b> (${sub})<br><span style="color:${szin}">${desc}</span></div>`;
-        }
-        if(items) html += `<h3>${d[type]}</h3>` + items;
-    }
-    document.getElementById('main-content').innerHTML = html;
-}
-
-function updateStyle() { document.body.className = document.getElementById('style-select').value; }
-function updateTheme() { updatePreview(); }
+// Az updateInterface() függvényben ellenőrizzük a label-eket
 function updateInterface() {
     const d = dictionary[currentLang];
-    for (let key in d) {
+    const labels = {
+        'lbl-lName': d.lName,
+        'lbl-fName': d.fName,
+        'lbl-streetName': d.streetName,
+        'lbl-type': "≡ " + d.type, // Itt adjuk hozzá az ikont programozottan
+        'lbl-house': d.house,
+        'lbl-cityHeader': d.cityHeader,
+        'lbl-atsTitle': d.atsTitle,
+        'lbl-ats1': d.ats1,
+        'lbl-ats2': d.ats2,
+        'lbl-ats3': d.ats3,
+        'lbl-ats4': d.ats4
+    };
+
+    for (let id in labels) {
+        const el = document.getElementById(id);
+        if (el) el.innerText = labels[id];
+    }
+    
+    // A többi label (Stílus, Szín, Telefon, stb.) frissítése...
+    const basicLabels = ['style', 'color', 'photo', 'phone', 'email', 'summary', 'license', 'edu', 'work', 'skills'];
+    basicLabels.forEach(key => {
         const el = document.getElementById('lbl-' + key);
         if (el) el.innerText = d[key];
-    }
+    });
 }
 function addEntry(type) {
     const div = document.createElement('div');

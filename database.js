50e387fd22192169a8209b3ead52bde63a0e3f8b
Csonka Norbert <norbi.markk2013@gmail.com>
2026-04-27 07:54:53 +0200

    Update database.js

--- database.js
+++ database.js
@@ -2,48 +2,27 @@ const careerDB = {
     eduLevels: [
         { hu: "Általános iskola", en: "Primary School", de: "Grundschule" },
         { hu: "Gimnáziumi érettségi", en: "High School Diploma", de: "Abitur" },
-        { hu: "Szakmunkás bizonyítvány", en: "Vocational Certificate", de: "Gesellenbrief" },
-        { hu: "Technikusi vizsga", en: "Technician Certificate", de: "Technikerabschluss" },
-        { hu: "OKJ: Raktárvezető", en: "Vocational: Warehouse Manager", de: "Lagerleiter Qualifikation" },
-        { hu: "Főiskolai diploma (BA/BSc)", en: "Bachelor's Degree", de: "Bachelor-Abschluss" },
-        { hu: "Egyetemi diploma (MA/MSc)", en: "Master's Degree", de: "Master-Abschluss" },
-        { hu: "Doktori fokozat (PhD)", en: "Doctorate Degree", de: "Doktortitel" },
-        { hu: "Logisztikai ügyintéző", en: "Logistics Assistant", de: "Logistikassistent" },
-        { hu: "Targoncavezető engedély", en: "Forklift Operator License", de: "Gabelstaplerschein" },
-        { hu: "Építőipari szakmunkás", en: "Construction Worker", de: "Bauarbeiter" },
-        { hu: "Vendéglátóipari végzettség", en: "Hospitality Qualification", de: "Gastronomie-Qualifikation" },
-        { hu: "Informatikai rendszergazda", en: "IT Systems Administrator", de: "IT-Systemadministrator" },
-        { hu: "Pénzügyi-számviteli ügyintéző", en: "Financial Clerk", de: "Finanzbuchhalter" },
-        { hu: "Nyelvvizsga (C1)", en: "Language Exam (C1)", de: "Sprachprüfung (C1)" }
-        // ... (A lista feltöltve 50-ig a leggyakoribb elemekkel)
+        { hu: "Szakmunkás bizonyítvány", en: "Vocational Certificate", de: "Berufsabschluss" },
+        { hu: "Főiskolai diploma (BSc)", en: "Bachelor's Degree", de: "Bachelor-Abschluss" },
+        { hu: "Egyetemi diploma (MSc)", en: "Master's Degree", de: "Master-Abschluss" },
+        { hu: "Szakács szakképesítés", en: "Chef Qualification", de: "Koch Qualifikation" },
+        // ... (Ez a lista a fájlodban 50 elemet tartalmazzon)
     ],
     jobs: [
         { hu: "Raktáros", en: "Warehouse Worker", de: "Lagerarbeiter" },
         { hu: "Teherautó sofőr", en: "Truck Driver", de: "LKW-Fahrer" },
-        { hu: "Rendszergazda", en: "IT Administrator", de: "Systemadministrator" },
-        { hu: "Eladó / Pénztáros", en: "Cashier", de: "Kassierer" },
+        { hu: "Eladó / Pénztáros", en: "Sales Assistant", de: "Verkäufer" },
         { hu: "Szakács", en: "Chef", de: "Koch" },
-        { hu: "Pincér", en: "Waiter", de: "Kellner" },
-        { hu: "Kőműves", en: "Bricklayer", de: "Maurer" },
-        { hu: "Villanyszerelő", en: "Electrician", de: "Elektriker" },
-        { hu: "Lakatos", en: "Locksmith", de: "Schlosser" },
-        { hu: "Adminisztrátor", en: "Administrator", de: "Administrator" },
-        { hu: "Projektmenedzser", en: "Project Manager", de: "Projektleiter" },
-        { hu: "Takarító", en: "Cleaner", de: "Reinigungskraft" },
-        { hu: "Biztonsági őr", en: "Security Guard", de: "Sicherheitskraft" },
-        { hu: "Könyvelő", en: "Accountant", de: "Buchhalter" },
-        { hu: "HR Generalista", en: "HR Generalist", de: "Personalreferent" },
-        { hu: "Futár", en: "Courier", de: "Kurier" },
-        { hu: "Gépkezelő", en: "Machine Operator", de: "Maschinenbediener" }
-        // ... (Feltöltve 50-ig a laboradatok szerint)
+        { hu: "Betanított munkás", en: "Skilled Worker", de: "Angelernter Arbeiter" },
+        { hu: "Rendszergazda", en: "System Administrator", de: "Systemadministrator" },
+        // ... (Ez a lista a fájlodban 50 elemet tartalmazzon)
     ],
     positions: [
-        { hu: "Alkalmazott", en: "Employee", de: "Angestellte(r)" },
-        { hu: "Csoportvezető", en: "Team Leader", de: "Teamleiter" },
+        { hu: "Alkalmazott", en: "Employee", de: "Angestellter" },
         { hu: "Műszakvezető", en: "Shift Supervisor", de: "Schichtleiter" },
-        { hu: "Üzletvezető", en: "Store Manager", de: "Filialleiter" },
-        { hu: "Manager", en: "Manager", de: "Manager" },
-        { hu: "Gyakornok", en: "Intern", de: "Praktikant" },
-        { hu: "Szakmai vezető", en: "Technical Lead", de: "Fachlicher Leiter" }
+        { hu: "Csoportvezető", en: "Team Leader", de: "Teamleiter" },
+        { hu: "Középvezető", en: "Middle Manager", de: "Abteilungsleiter" },
+        { hu: "Gyakornok", en: "Intern", de: "Praktikant" }
+        // ... (Ez a lista a fájlodban 50 elemet tartalmazzon)
     ]
 };
